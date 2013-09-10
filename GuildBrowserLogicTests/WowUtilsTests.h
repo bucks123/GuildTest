1572e4850b5087c6a13322d5623855b3bacd1d34
Kieran Buckley <kierantbuckley@gmail.com>
2013-09-10 14:07:40 +0100

    update

---
+++ GuildBrowserLogicTests/WowUtilsTests.h
@@ -0,0 +1,13 @@
+//
+//  WowUtilsTests.h
+//  GuildBrowser
+//
+//  Created by kieran buckley on 10/09/13.
+//  Copyright (c) 2013 Charlie Fulton. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface WowUtilsTests : SenTestCase
+
+@end
---
+++ GuildBrowserLogicTests/WowUtilsTests.m
@@ -0,0 +1,40 @@
+//
+//  WowUtilsTests.m
+//  GuildBrowser
+//
+//  Created by kieran buckley on 10/09/13.
+//  Copyright (c) 2013 Charlie Fulton. All rights reserved.
+//
+
+#import "WowUtilsTests.h"
+#import "WoWUtils.h"
+
+@implementation WowUtilsTests
+
+-(void)testCharachterClassNameLookup{
+    
+    STAssertEqualObjects(@"Warrior", [WoWUtils classFromCharacterType:1], @"ClassType should be Warrior");
+    
+    STAssertFalse(@"Mage" isEqualToString:[WoWUtils classFromCharacterType:2], nil);
+    
+    STAssertTrue(@"Paladine" isEqualToString:[WoWUtils classFromCharacterType:2], nil);
+    
+    
+}
+
+- (void)testRaceTypeLookup
+{
+    STAssertEqualObjects(@"Human", [WoWUtils raceFromRaceType:1], nil);
+    STAssertEqualObjects(@"Orc", [WoWUtils raceFromRaceType:2], nil);
+    STAssertFalse([@"Night Elf" isEqualToString:[WoWUtils raceFromRaceType:45]],nil);
+    // add the rest as an exercise
+}
+
+- (void)testQualityLookup
+{
+    STAssertEquals(@"Grey", [WoWUtils qualityFromQualityType:1], nil);
+    STAssertFalse([@"Purple" isEqualToString:[WoWUtils qualityFromQualityType:10]],nil);
+    // add the rest as an exercise
+}
+
+@end
