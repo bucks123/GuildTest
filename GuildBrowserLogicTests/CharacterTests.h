94034361880d42f04e5b3c307b00daf0c20700ae
Kieran Buckley <kierantbuckley@gmail.com>
2013-09-14 11:00:28 +0100

    update to tests

---
+++ GuildBrowserLogicTests/CharacterTests.h
@@ -0,0 +1,13 @@
+//
+//  CharacterTests.h
+//  GuildBrowser
+//
+//  Created by kieran buckley on 11/09/13.
+//  Copyright (c) 2013 Charlie Fulton. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface CharacterTests : SenTestCase
+
+@end
